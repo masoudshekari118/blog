ef01547c5f3115b623efc68c6bc7c037d6bb2324
mshekari <mshekari95@gmail.com>
2025-06-07 01:41:19 -0700

    Update Footer.tsx

--- quartz/components/Footer.tsx
+++ quartz/components/Footer.tsx
@@ -15,7 +15,6 @@ export default ((opts?: Options) => {
       <footer class={`${displayClass ?? ""}`}>
         <p>
             کلیه حقوق سایت متعلق به مسعود شکری است. {" "}
-          <a href="https://quartz.jzhao.xyz/">Quartz v{version}</a> © {year}
         </p>
         <ul>
           {Object.entries(links).map(([text, link]) => (
