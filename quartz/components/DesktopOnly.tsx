7592b62cb390b2dfac1f971a6b85a8d65c3f238c
mshekari <mshekari95@gmail.com>
2025-06-07 07:22:47 -0700

    Update DesktopOnly.tsx

--- quartz/components/DesktopOnly.tsx
+++ quartz/components/DesktopOnly.tsx
@@ -1,38 +1,14 @@
-// components for pages that display a single page (e.g. a single note)
-export const defaultContentPageLayout: PageLayout = {
-  beforeBody: [
-  Component.Breadcrumbs(),
-  Component.ArticleTitle(),
-  Component.ContentMeta(),
-  Component.TagList(),
-  ],
-  left: [
-  Component.DesktopOnly(Component.Graph()),
-  Component.DesktopOnly(Component.Backlinks()),
-  Component.MobileOnly(Component.PageTitle()),
-  Component.MobileOnly(Component.Darkmode()),
-  ],
-  right: [
-  Component.DesktopOnly(Component.PageTitle()),
-  Component.Search(),
-  Component.DesktopOnly(Component.Darkmode()),
-  Component.DesktopOnly(Component.Explorer()),
-  Component.DesktopOnly(Component.TableOfContents()),
-  Component.MobileOnly(Component.Backlinks()),
-  ],
-}
- 
-// components for pages that display lists of pages  (e.g. tags or folders)
-export const defaultListPageLayout: PageLayout = {
- beforeBody: [Component.Breadcrumbs(), Component.ArticleTitle(), Component.ContentMeta()],
-  left: [
-  Component.MobileOnly(Component.PageTitle()),
-  Component.MobileOnly(Component.Darkmode()),
-  ],
-  right: [
-  Component.DesktopOnly(Component.PageTitle()),
-  Component.Search(),
-  Component.DesktopOnly(Component.Darkmode()),
-  Component.DesktopOnly(Component.Explorer()),
-  ],
-}
+import { QuartzComponent, QuartzComponentConstructor, QuartzComponentProps } from "./types"
+
+export default ((component: QuartzComponent) => {
+  const Component = component
+  const DesktopOnly: QuartzComponent = (props: QuartzComponentProps) => {
+    return <Component displayClass="desktop-only" {...props} />
+  }
+
+  DesktopOnly.displayName = component.displayName
+  DesktopOnly.afterDOMLoaded = component?.afterDOMLoaded
+  DesktopOnly.beforeDOMLoaded = component?.beforeDOMLoaded
+  DesktopOnly.css = component?.css
+  return DesktopOnly
+}) satisfies QuartzComponentConstructor<QuartzComponent>
